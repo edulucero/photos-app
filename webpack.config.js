282721ef85990c30eb8eb142b26a35834a5a124c
edulucero <edu.m.lucero@gmail.com>
2019-09-13 21:06:35 +1000

    Added _redirects for refreshing

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
  module.exports = {
   entry: './app/index.js',
@@ -18,7 +19,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
   plugins: [
     new HtmlWebpackPlugin({
       template: 'app/index.html'
-    })
+    }),
+    new CopyPlugin([
+      { from: '_redirects'}
+    ])
   ],
   devServer: {
     historyApiFallback: true 
